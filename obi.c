02a2c16e6977bac51d675b572b024907857ce38f
kalwhyte <superkalel55@gmail.com>
2023-06-09 18:54:38 +0000

    engineering

---
+++ obi.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point.
+ * Return: Always return zero or 0.
+ */
+
+int main(void)
+{
+	//printf("Hello, World!"/n);
+	printf("Hello, World!\n");
+	printf("Hello, World!\n");
+	return (0);
+}
---
+++ raheema.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always return 0.
+ */
+int main() { 
+	//printf("result/n");
+	int sule = 1;
+	char result[] = "Hello";
+	printf("%d %s\n", sule, result);
+
+	return 0;
+}
